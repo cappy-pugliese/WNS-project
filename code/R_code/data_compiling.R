fd797df5c903a9c15047d8d93afebdaf7a27776b
Cappy <cappy.pugliese@gmail.com>
2025-10-16 14:35:28 -0400

    working on getting lat long data

---
+++ code/R_code/data_compiling.R
@@ -0,0 +1,4 @@
+setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/01_pd-samples")
+
+pd_samples <- read.csv("25_10-16_pd_samples-edited.csv")
+paper_samples <- read.csv("strain_list.csv")
