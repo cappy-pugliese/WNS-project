e9b134e3a5249b0c398c028e775b52b3e2fb0856
Cappy <cappy.pugliese@gmail.com>
2025-10-16 10:56:26 -0400

    created a separate R file for the actual plot portion

--- code/R_code/pcangsd_graph.R
+++ code/R_code/pcangsd_graph.R
@@ -1,38 +1,8 @@
-#install.packages("reticulate")
-library("reticulate")
-# to help read .npy file
-np <- import("numpy")
-# good for combining python with R
-library(readr)
-library(tidyverse)
 library(ggplot2)
 library(see)
-library(vcfR)
 
 setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/04_pcangsd")
-
-admix <- np$load(file = "pcangsd_pd.admix.6.Q.npy",allow_pickle=FALSE)
-k <- 6
-admix= as.data.frame(admix)
-#head(admix)
-
-########## making the graph ##########
-pops <- c()
-for (n in 1:k) {
-    pops <- c(pops, paste0("pop", n))
-}
-colnames(admix) <- pops
-
-
-indivs <- read.csv("ind_ids.txt")
-
-
-rownames(admix) = indivs$individuals
-admix$ind = indivs$individuals
-
-
-#Pivot to long format
-df_long = pivot_longer(admix,1:k,names_to="Pop",values_to="admix")
+df_long <- read.csv("25_10-16_pd_pcangsd_longdf")
 
 ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
 scale_fill_okabeito(
