cb5f84e0b15a5ef8ac034afb504efcaa79b7ac41
Cappy <cappy.pugliese@gmail.com>
2025-10-16 21:52:57 -0400

    added new csv

--- code/R_code/data_compiling.R
+++ code/R_code/data_compiling.R
@@ -5,3 +5,5 @@ setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis
 pd_samples <- read.csv("25_10-16_pd_samples-edited.csv")
 
 pd_locations <- pd_samples |> dplyr::select(internal_line_id, continent, country, state, year)
+
+write.csv(pd_locations,file="25_10-16_pd_locations.csv",row.names=FALSE,quote=FALSE)
