9ebc360084709621bb1092517eb11884bd528074
Cappy <cappy.pugliese@gmail.com>
2025-11-05 16:18:42 -0500

    banging my head against the wall

--- code/scripts/12_pipe2slurm.sh
+++ code/scripts/12_pipe2slurm.sh
@@ -1,14 +1,14 @@
 #This script takes the commands produced by raw2gvcf.sh and splits them into individual jobs per sample. Allows for quick modification of parameters for all of the runs.
 awk '{if(gsub(/##/,"")) {name=$0;
-        print "#!/bin/bash" >> name".sbatch";
+        print "#!/bin/bash" > name".sbatch";
         print "#SBATCH -N 1" >> name".sbatch";
         print "#SBATCH -n 1" >> name".sbatch";
-        print "#SBATCH -c 2" >> name".sbatch";
+        print "#SBATCH -c 4" >> name".sbatch";
         print "#SBATCH --qos=general" >> name".sbatch";
         print "#SBATCH --partition=general" >> name".sbatch";
         print "#SBATCH --ntasks 1" >> name".sbatch";
         print "#SBATCH -t 00:20:00 " >> name".sbatch";
-        print "#SBATCH --mem 2G " >> name".sbatch";
+        print "#SBATCH --mem 3G " >> name".sbatch";
         print "#SBATCH -o logs/"name ".%j.out" >> name".sbatch";
         print "#SBATCH -e logs/"name ".%j.err" >> name".sbatch";
         print "#SBATCH -J " name >> name".sbatch";}
--- code/scripts/16_parallel_iqtree.sh
+++ code/scripts/16_parallel_iqtree.sh
@@ -18,8 +18,13 @@ date
 source ~/.bashrc
 module load bcftools/1.20
 module load iqtree/3.0.1
+module load parallel/20180122
 
 ## set variables
 WIND=/home/FCAM/cpugliese/wns/06_iqtree/test_wind.txt
-VCF=/home/FCAM/cpugliese/lab_wns/05_vcfs/03_filtered-vcfs/pd_filtered.vcf.gz
 TMP=/home/FCAM/cpugliese/wns/06_iqtree/02_test-run
+
+cd $TMP
+cat $WIND | parallel -j 2 sbatch
+
+########### script end
