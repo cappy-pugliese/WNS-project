05446050380fc02e89041d89b6739ade68e3b7e1
Cappy <cappy.pugliese@gmail.com>
2025-11-13 22:16:56 -0500

    added new graph, updated data

--- code/R_code/03_pcangsd_graph.R
+++ code/R_code/03_pcangsd_graph.R
@@ -14,6 +14,8 @@ scale_fill_okabeito(
   order = 1:6,
   aesthetics = "fill",
   ) +
+geom_col(col=NA,inherit.aes = TRUE) +
+theme(axis.text.x = element_text(angle = 90, hjust = 1, size=5)) +
 geom_col(col=NA,inherit.aes = TRUE)
 
 # graph looking at continent population admixture
@@ -57,3 +59,17 @@ scale_fill_okabeito(
   ) +
 theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
 geom_col(col=NA,inherit.aes = TRUE)
+
+
+## grouped by continent graph
+ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
+scale_fill_okabeito(
+  palette = "full",
+  reverse = FALSE,
+  order = 1:6,
+  aesthetics = "fill",
+  ) +
+geom_col(col=NA,inherit.aes = TRUE) +
+theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8)) +
+geom_col(col=NA,inherit.aes = TRUE) +
+facet_wrap( ~ continent, strip.position = "bottom", scales = "free_x")
