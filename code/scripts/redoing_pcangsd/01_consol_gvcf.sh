79b13091ab83db0e8fbf12c1ae212a14d0b201c9
Cappy <cappy.pugliese@gmail.com>
2025-12-01 20:30:03 -0500

    added scripts

---
+++ code/scripts/redoing_pcangsd/01_consol_gvcf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=consol_GVCF
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 5
+#SBATCH --mem=10G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+########### script start
+
+hostname
+date
+
+## load modules
+module load GATK/4.3.0.0
+
+#### point to paths
+INDIR=/home/FCAM/cpugliese/lab_wns/04_gvcfs/01_haplotypecaller/02_only-pd
+OUTDIR=/home/FCAM/cpugliese/lab_wns/04_gvcfs/02_consolidate-gvcfs/02_only-pd
+INTLIST=/home/FCAM/cpugliese/wns/02_raw-data/pd_data/pd_ref/pd_ref.bed
+
+cd $INDIR
+gatk --java-options "-Xmx4g -Xms4g" \
+       GenomicsDBImport \
+       --genomicsdb-workspace-path $OUTDIR \
+       --sample-name-map $INDIR/Pd_sample_map.txt \
+       --reader-threads 5 \
+       -L $INTLIST
+       
+########### script end
