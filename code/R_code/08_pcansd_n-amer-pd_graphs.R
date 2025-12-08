439e233551f825efe1b6def42c44e1e18fd87305
Cappy <cappy.pugliese@gmail.com>
2025-12-08 12:55:12 -0500

    fixed location data typo

--- code/R_code/08_pcansd_n-amer-pd_graphs.R
+++ code/R_code/08_pcansd_n-amer-pd_graphs.R
@@ -3,7 +3,7 @@ library(see)
 library(dplyr)
 
 setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/04_pcangsd")
-df_long <- read.csv("25_12-07_only-pd_pcangsd_longdf.csv")
+df_long <- read.csv("25_12-08_n-amer-pd_pcangsd_longdf.csv")
 df_continent <- df_long |> group_by(continent) |> arrange(.by_group = TRUE)
 
 # og graph
@@ -13,10 +13,18 @@ geom_col(col=NA,inherit.aes = TRUE) +
 theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.5, 'cm')) +
 geom_col(col=NA,inherit.aes = TRUE)
 
-## grouped by continent graph
+## grouped by country graph
 ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
 scale_fill_viridis_d() +
 geom_col(col=NA,inherit.aes = TRUE) +
 theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.3, 'cm')) +
 geom_col(col=NA,inherit.aes = TRUE) +
-facet_wrap( ~ continent, strip.position = "bottom", scales = "free_x")
+facet_wrap( ~ country, strip.position = "bottom", scales = "free_x")
+
+## grouped by state graph
+ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
+scale_fill_viridis_d() +
+geom_col(col=NA,inherit.aes = TRUE) +
+theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.3, 'cm')) +
+geom_col(col=NA,inherit.aes = TRUE) +
+facet_wrap( country ~ state, strip.position = "bottom", scales = "free_x")
