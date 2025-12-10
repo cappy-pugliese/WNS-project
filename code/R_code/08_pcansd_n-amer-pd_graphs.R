1add1752b513f4e4bb991d2cb04bb30fa0f3b341
Cappy <cappy.pugliese@gmail.com>
2025-12-10 16:48:08 -0500

    synced R scripts with popgen proj, made some modifications

--- code/R_code/08_pcansd_n-amer-pd_graphs.R
+++ code/R_code/08_pcansd_n-amer-pd_graphs.R
@@ -1,45 +1,36 @@
 library(ggplot2)
 library(see)
 library(dplyr)
-library(ggh4x)
 
 setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/04_pcangsd")
 df_long <- read.csv("25_12-09_n-amer-pd_pcangsd_longdf.csv")
-df_continent <- df_long |> group_by(continent) |> arrange(.by_group = TRUE)
+
+cols <- c("#064061", "#0072B2", "#56B4E9", "#B4E1FB", "#81BFAE", "#009E73","#DACE1E","#FDF17B","#E69F00","#D55E00","#9C4907","#F5A9D3")
 
 # og graph
 ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
-scale_fill_viridis_d() +
+scale_fill_manual(values = rev(cols)) +
 geom_col(col=NA,inherit.aes = TRUE) +
 theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.5, 'cm')) +
 geom_col(col=NA,inherit.aes = TRUE) +
 labs(title = "North American Pd Samples PCAngsd Admixture", x = "Individuals", y = "Admix") 
 
 ## grouped by country graph
-ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
-scale_fill_viridis_d() +
+plot2 <- ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
+scale_fill_manual(values = rev(cols)) +
 geom_col(col=NA,inherit.aes = TRUE) +
 theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.3, 'cm')) +
 geom_col(col=NA,inherit.aes = TRUE) +
 facet_wrap( ~ country, strip.position = "bottom", scales = "free_x") +
 labs(title = "North American Pd Samples by Country", x = "Individuals", y = "Admix") 
+plot2
+#cvd_grid(plot2)
 
-## grouped by state graph
-df_canada = df_long |> dplyr::filter(country=="Canada")
-df_usa = df_long |> dplyr::filter(country=="USA")
-
-# canada by state
-ggplot(df_canada,aes(x=ind,y=admix,fill=Pop)) +
-scale_fill_viridis_d() +
+## grouped by year
+ggplot(df_long,aes(x=ind,y=admix,fill=Pop)) +
+scale_fill_manual(values = rev(cols)) +
 geom_col(col=NA,inherit.aes = TRUE) +
 theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.3, 'cm')) +
 geom_col(col=NA,inherit.aes = TRUE) +
-facet_grid2(~ state, scales = "free_x", space= "free_x")
-
-# usa by state
-ggplot(df_usa,aes(x=ind,y=admix,fill=Pop)) +
-scale_fill_viridis_d() +
-geom_col(col=NA,inherit.aes = TRUE) +
-theme(axis.text.x = element_text(angle = 90, hjust = 1, size=8), legend.key.size=unit(0.3, 'cm')) +
-geom_col(col=NA,inherit.aes = TRUE) +
-facet_grid2(~ state, scales = "free_x", space= "free_x")
+facet_grid( ~ year, scales = "free_x", space="free_x", switch = "x") +
+labs(title = "North American Pd Samples by Year", x = "Individuals Separated by Year", y = "Admix") 
