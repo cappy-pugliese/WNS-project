c1a8a96621a6221467faf5a94798c196f1c9beda
Cappy <cappy.pugliese@gmail.com>
2026-01-26 17:08:45 -0500

    updated algatr files, added code_citations, added new plink.sh to get ld

--- code/R_code/11_algatr.R
+++ code/R_code/11_algatr.R
@@ -9,26 +9,26 @@ raster_path = "/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School
 coords <- read.csv("data/05_algatr/NoA_Pd_coords.csv")
 vcf <- read.vcfR("data/05_algatr/n-amer-no-clones_filtered.vcf")
 
-
+# load bioclimatic data
 bio <- worldclim_global(path=raster_path, var="bio", res=2.5)
 
 # crop Raster* with Spatial* object
 NoA <- as(extent(-135, -55, 25, 60), 'SpatialPolygons')
 crs(NoA) <- crs(bio)
 NoA_bio <- crop(bio, NoA)
-plot(NoA_bio[[12]])
+# plot(NoA_bio[[12]])
 
 ## https://stackoverflow.com/questions/75739085/running-a-pca-on-a-rasterstack-in-r
 ## pca code
 pca <- prcomp(NoA_bio)
 x <- predict(NoA_bio, pca)
-plot(x)
+# plot(x)
 
 NoA_pca_map <- plotRGB(scaleRGB(x),r=1,g=2,b=3)
 
 
 ####
-do_everything_for_me(vcf, coords, NoA_pca_map)
+do_everything_for_me(Pd_vcf, coords, NoA_pca_map)
 #Please be aware: the do_everything functions are meant to be exploratory. We do not recommend their use for final analyses unless certain they are properly parameterized.
 #Loading required namespace: adegenet
 #Registered S3 method overwritten by 'ecodist':
@@ -39,3 +39,5 @@ do_everything_for_me(vcf, coords, NoA_pca_map)
 #  NAs found in genetic data, imputing to the median (NOTE: this simplified imputation approach is strongly discouraged. Consider using another method of removing missing data)
 #Error:
 #! vector memory limit of 16.0 Gb reached, see mem.maxVSize()
+
+### looks like my computer does not have enough memory to run this lol
---
+++ code/code_citations.md
@@ -0,0 +1,18 @@
+# Code Citations
+
+## Cluster programs
+### pcangsd
+
+### plink
+
+### bcftools
+
+### iqtree
+
+
+## R programs
+### algatr
+**citation needed**
+
+### TESS software
+Caye K., Deist T.M., Martins H., Michel O., François O. (2016) TESS3: fast inference of spatial population structure and genome scans for selection. Molecular Ecology Resources 16(2):540-548. DOI: 10.1111/1755-0998.12471.
