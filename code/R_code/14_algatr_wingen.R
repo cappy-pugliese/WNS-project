219e6e7dd36ee42994590020f60ac7f121f52844
Cappy <cappy.pugliese@gmail.com>
2026-02-04 15:16:09 -0500

    had gotten things mixed around, fixed

--- code/R_code/14_algatr_wingen.R
+++ code/R_code/14_algatr_wingen.R
@@ -2,6 +2,5 @@ R_code_path <- "/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_Schoo
 source(paste0(R_code_path, "12_algatr_src.R"))
 
 #libraries
-mmrr_packages()
-library(here)
-
+wingen_packages()
+library(wingen)
