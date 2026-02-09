8e889715f2ded93a7cbed42361592e2cd933f88c
Cappy <cappy.pugliese@gmail.com>
2026-02-09 18:19:05 -0500

    new r doc for gdm

---
+++ code/R_code/16_algatr_GDM.R
@@ -0,0 +1,10 @@
+R_code_path <- "/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/code/R_code/"
+source(paste0(R_code_path, "12_algatr_src.R"))
+
+#libraries
+gdm_packages()
+library(gdm)
+
+env
+
+
