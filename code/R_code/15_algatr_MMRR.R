1964849b660639124da7937ef2316c3b32576515
Cappy <cappy.pugliese@gmail.com>
2026-02-09 17:57:54 -0500

    made mmrr plots

--- code/R_code/15_algatr_MMRR.R
+++ code/R_code/15_algatr_MMRR.R
@@ -6,5 +6,16 @@ mmrr_packages()
 library(here)
 
 Y <- as.matrix(euc_dists)
-X <- env_dist
+X <- as.matrix(env_dist)
 X[["geodist"]] <- geo_dist(coords_longlat)
+
+results_full <- mmrr_run(Y, X, nperm = 99, stdz = TRUE, model = "full")
+results_best <- mmrr_run(Y, X, nperm = 99, stdz = TRUE, model = "best")
+# x-axis is the environmental distance
+# y-axis is the genetic distance
+
+mmrr_plot(Y, X, mod = results_full$mod, plot_type = "all", stdz = TRUE)
+mmrr_table(results_full, digits = 2, summary_stats = TRUE)
+
+mmrr_plot(Y, X, mod = results_best$mod, plot_type = "all", stdz = TRUE)
+mmrr_table(results_best, digits = 2, summary_stats = TRUE)
