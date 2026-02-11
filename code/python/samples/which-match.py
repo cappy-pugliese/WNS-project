26731f417a7306ff45d2e721e76907f5bb465faa
Cappy <cappy.pugliese@gmail.com>
2026-02-10 22:54:03 -0500

    added python folder and script

---
+++ code/python/samples/which-match.py
@@ -0,0 +1,26 @@
+import pandas as pd
+#import os
+#os.getcwd()
+#os.chdir('/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/code/python/samples/')
+
+with open('sample-list.txt', 'r') as file:
+    lines = file.readlines()
+
+pd_samples = []
+for i, line in enumerate(lines, start=1):
+    line = line.strip()
+    parts = line.split("_")
+    # get "Song Title"
+    number = int(parts[1].strip())
+    pd_samples.append(number)
+
+df_pd_samples = pd.DataFrame(pd_samples)
+
+total_samps = []
+for x in range(85):
+    tot = x+1
+    total_samps.append(tot)
+
+df_tot_samps = pd.DataFrame(total_samps)
+
+### finish the tutorial from here
