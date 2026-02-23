fe9067740a9958de0e4518955e3848fcb492b59b
Cappy <cappy.pugliese@gmail.com>
2026-02-23 12:34:05 -0500

    added pca.R and reorganized R code

---
+++ code/R_code/17_pca-plot.R
@@ -0,0 +1,22 @@
+R_code_path <- "/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/code/R_code/"
+source(paste0(R_code_path, "12_algatr_src.R"))
+
+setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/04_pcangsd/04_n-amer-no-washington")
+
+#FeCu_centered_scaled <- scale(FeCu_centered, center = FALSE, scale = TRUE)
+
+## how genetically far apart the individuals are: euc_dists
+## how geographically far apart the individuals are: geo_dist
+
+
+## pcangsd tutorial
+pop<-read.table("Demo1pop.info")
+cov <- as.matrix(read.table("pcangsd_n-amer-no-washington.cov"))
+e <- eigen(cov)
+
+pdf("PCAngsd1.pdf")
+plot(e$vectors[,1:2],col=pop[,1],xlab="PC1",ylab="PC2", main="individual allele frequency")
+legend("top",fill=1:5,levels(pop[,1]))
+dev.off()
+
+plot(e$vectors[,1:2],xlab="PC1",ylab="PC2", main="individual allele frequency")
