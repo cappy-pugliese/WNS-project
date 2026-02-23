081b71ed475f46f25556b9f2ff4e93583f425430
Cappy <cappy.pugliese@gmail.com>
2026-02-23 13:02:28 -0500

    added R doc for making new data table, updated/added data tables

---
+++ code/R_code/01_pcangsd/11_pca_no-washington_data.R
@@ -0,0 +1,14 @@
+R_code_path <- "/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/code/R_code/02_algatr"
+source(paste0(R_code_path, "12_algatr_src.R"))
+
+setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/04_pcangsd/04_n-amer-no-washington")
+
+############
+pd_locations <- read.csv("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/01_pd-samples/26_02-23_n-amer-no-washington_locations.csv")
+coords
+pd_locations$long <- coords$x
+pd_locations$lat <- coords$y
+
+
+############
+write.csv(pd_locations,file="26_02-23_n-amer-no-washington_pca-info.csv",row.names=FALSE,quote=FALSE)
