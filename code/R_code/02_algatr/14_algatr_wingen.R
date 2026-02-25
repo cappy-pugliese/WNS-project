b3cd86d95a822576df1ef7b689432bdd0ee56346
Cappy <cappy.pugliese@gmail.com>
2026-02-25 17:32:55 -0500

    finished wingen, started installing feems on cluster

--- code/R_code/02_algatr/14_algatr_wingen.R
+++ code/R_code/02_algatr/14_algatr_wingen.R
@@ -31,14 +31,35 @@ wgd <- window_gd(ld_pruned_vcf,
 # Plot map of pi
 ggplot_gd(wgd, bkg = envlayer) + ggtitle("Moving window pi")
 
-setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/05_algatr")
-kgd <- terra::rast("wingen_kgd.tif")
-#kgd <- krig_gd(wgd, index = 1:2, pd_lyr, disagg_grd = 5)
+#setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/05_algatr")
+#kgd <- terra::rast("wingen_kgd_no-washington.tif")
+kgd <- krig_gd(wgd, index = 1:2, pd_lyr, disagg_grd = 5)
 # worked, but also produced warnings + took long time
 # Warning messages:
   #1: `krig_gd()` was deprecated in wingen 2.2.0. ℹ Please use `wkrig_gd()` instead. This warning is displayed once per session. Call lifecycle::last_lifecycle_warnings() to see where this warning was generated. 
   #2: In sqrt(krige_result$var1.var) : NaNs produced
   #3: In sqrt(krige_result$var1.var) : NaNs produced
-#setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/05_algatr")
+setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/05_algatr")
 # S4 method for class 'SpatRaster,character'
-#writeRaster(kgd, "wingen_kgd.tif", overwrite=FALSE)
+writeRaster(kgd, "wingen_kgd_no-washington.tif", overwrite=FALSE)
+
+# Plot kriged map of pi
+ggplot_gd(kgd) + ggtitle("Kriged pi")
+
+# Plot kriged sample count map
+ggplot_count(kgd) + ggtitle("Kriged sample counts")
+
+# adding a mask using sample counts
+mgd_1 <- mask_gd(kgd, kgd[["sample_count"]], minval = 1)
+ggplot_gd(mgd_1, bkg = envlayer) + ggtitle("Kriged & masked pi")
+
+
+# mask using boundary raster file
+## Resample envlayer based on masked layer
+r <- terra::resample(envlayer, mgd_1)
+
+## Perform masking
+mgd <- mask_gd(mgd_1, r)
+
+## Plot masked map
+ggplot_gd(mgd, bkg = envlayer) + ggtitle("Kriged & masked pi")
