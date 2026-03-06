6d2c59597e35220a3872c6c572b03905356aa532
Cappy <cappy.pugliese@gmail.com>
2026-03-06 14:08:51 -0500

    updated

--- code/R_code/03_pixy/01_n-amer-no-clones/03_pixy_by-year.R
+++ code/R_code/03_pixy/01_n-amer-no-clones/03_pixy_by-year.R
@@ -13,11 +13,6 @@ dxy_df2 <- dxy_df |> mutate(.before=pop1 ,row_order = as.numeric(gsub("\\D+", ""
 
 fst_df2 <- fst_df |> mutate(.before=pop1 , row_order = as.numeric(gsub("\\D+", "",(paste(chromosome,window_pos_1,pop1,pop2, sep = "_"))))) |> arrange(row_order)
 
-## brain thoughts on what to do
-### - use dplyr to merge pop1 and pop2 columns into one
-### - maybe can do something like turn pop8 and pop3 into 83, that way it is easier to sort by order?
-### - first order by window_pos_1 and then by pop_compar number?
-
 dxy_df3 <- dxy_df2 |> select(row_order,avg_dxy,no_sites,count_diffs,count_comparisons,count_missing)
 
 merged_fst_dxy <- left_join(fst_df2, dxy_df3, by = "row_order")
@@ -27,5 +22,7 @@ selected_fst_dxy <- merged_fst_dxy |> select(row_order, pop1, pop2, chromosome, 
 # conditional statement: sets fst NAs to zero if dxy is also zero
 selected_fst_dxy$avg_hudson_fst[is.na(selected_fst_dxy$avg_hudson_fst) & selected_fst_dxy$avg_dxy == 0] <- 0
 
-selected_fst_dxy |> summarise(across(avg_hudson_fst, ~ sum(is.na(.))))
-merged_fst_dxy |> summarise(across(avg_hudson_fst, ~ sum(is.na(.))))
+#selected_fst_dxy |> summarise(across(avg_hudson_fst, ~ sum(is.na(.))))
+#merged_fst_dxy |> summarise(across(avg_hudson_fst, ~ sum(is.na(.))))
+# fixed 5663333 NAs, still have 27216 NAs
+# got rid of 99.5% of the NAs though lol
