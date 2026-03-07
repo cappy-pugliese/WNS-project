0a48817b885025139cd20f352005ea8fb62278d1
Cappy <cappy.pugliese@gmail.com>
2026-03-06 20:57:09 -0500

    updated

--- code/R_code/03_pixy/01_n-amer-no-clones/04_graph_fst_by-year.R
+++ code/R_code/03_pixy/01_n-amer-no-clones/04_graph_fst_by-year.R
@@ -25,13 +25,9 @@ violin_plot <- ggplot(data=pop1_2, aes(x=genome_group, y=avg_hudson_fst)) +
   labs(title = "Fst for Year1 vs Year2", x = "Moving across the genome", y = "Average Fst")
 #ggplot(fst_dxy_df, aes(x=window_pos_1, y=)
 
-pop1_compar_df <- fst_dxy_df |> mutate(.before=pop1 , year_compar = paste(as.character(as.numeric(gsub("\\D+", "",(paste(pop1)))) + 2007), as.character(as.numeric(gsub("\\D+", "",(paste(pop2)))) + 2007), sep="_")) |> arrange(row_order) |> filter(pop1=="pop1" | pop2 =="pop1")
+pop1_compar_df <- fst_dxy_df |> mutate(.before=pop1 , year_compar = as.character((abs((as.numeric(gsub("\\D+", "",(paste(pop1)))) + 2007) - (as.numeric(gsub("\\D+", "",(paste(pop2)))) + 2007)) + 1) + 2007)) |> filter(pop1=="pop1" | pop2 =="pop1")
 
 year1_compar_plot <- ggplot(data=pop1_compar_df, aes(x=year_compar, y=avg_hudson_fst)) +
   geom_violin() +
   labs(title = "Fst for 2008 vs all other years, North America Samples", x = "Year Comparisons", y = "Average Fst")
 year1_compar_plot
-
-
-
-pop1_compar_df <- fst_dxy_df |> mutate(.before=pop1 , year_compar = as.character((abs((as.numeric(gsub("\\D+", "",(paste(pop1)))) + 2007) - (as.numeric(gsub("\\D+", "",(paste(pop2)))) + 2007)) + 1) + 2007)) |> filter(pop1=="pop1" | pop2 =="pop1")
