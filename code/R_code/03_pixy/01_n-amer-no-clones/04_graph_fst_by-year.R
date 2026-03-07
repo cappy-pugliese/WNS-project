881bd4321b05f30b21b61e7fe59e2aa3d27ef5b5
Cappy <cappy.pugliese@gmail.com>
2026-03-06 20:24:24 -0500

    updated code

--- code/R_code/03_pixy/01_n-amer-no-clones/04_graph_fst_by-year.R
+++ code/R_code/03_pixy/01_n-amer-no-clones/04_graph_fst_by-year.R
@@ -6,26 +6,28 @@ setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis
 fst_dxy_df <- read.csv("26_03-06_fst_dxy_no_nas_10000.csv")
 
 pop1_2_summary <- fst_dxy_df |> filter(pop1 == "pop1" & pop2 == "pop2") |> group_by(chromosome) |> summarize(avg = mean(avg_hudson_fst))
+pop1_2_summary$x <- 1:83
 
 pop1_2 <- fst_dxy_df |> filter(pop1 == "pop1" & pop2 == "pop2") |> arrange(chromosome,window_pos_1)
 
-pop1_2_summary$x <- 1:83
+pop1_2 <- pop1_2 |> mutate(.before=pop1, window_order = (paste(chromosome,window_pos_1, sep = "_"))) |> select(window_order, pop1, pop2, chromosome, avg_hudson_fst, avg_dxy)
 
+pop1_2 <- pop1_2 |> 
+  mutate(genome_group = factor(ceiling(row_number() / 968)))
 
-
-pop1_2 <- pop1_2 |> mutate(.before=pop1 , window_order = (paste(chromosome,window_pos_1, sep = "_"))) |> select(window_order, pop1, pop2, chromosome, avg_hudson_fst, avg_dxy)
-
-test_df <- pop1_2 |> 
-  mutate(group_1000 = (row_number() - 1) / 968 + 1)
-
-test_df <- df %>%
-  mutate(group_1000 = (row_number() - 1) %/% 1000 + 1)
 
 ggplot(data=pop1_2_summary, aes(x=x,y=avg)) +
   geom_point() +
   labs(title = "Fst for Year1 vs Year2", x = "genome scaffolds", y = "Average Fst") 
 
-ggplot(data=pop1_2, aes(x=chromosome, y=avg_hudson_fst)) +
+violin_plot <- ggplot(data=pop1_2, aes(x=genome_group, y=avg_hudson_fst)) +
   geom_violin() +
-  geom_dotplot(binaxis='y', stackdir='center', dotsize=0.1)
+  labs(title = "Fst for Year1 vs Year2", x = "Moving across the genome", y = "Average Fst")
 #ggplot(fst_dxy_df, aes(x=window_pos_1, y=)
+
+pop1_compar_df <- fst_dxy_df |> mutate(.before=pop1 , year_compar = paste(as.character(as.numeric(gsub("\\D+", "",(paste(pop1)))) + 2007), as.character(as.numeric(gsub("\\D+", "",(paste(pop2)))) + 2007), sep="_")) |> arrange(row_order) |> filter(pop1=="pop1" | pop2 =="pop1")
+
+year1_compar_plot <- ggplot(data=pop1_compar_df, aes(x=year_compar, y=avg_hudson_fst)) +
+  geom_violin() +
+  labs(title = "Fst for Year1 vs all others", x = "Year Comparisons", y = "Average Fst")
+year1_compar_plot
