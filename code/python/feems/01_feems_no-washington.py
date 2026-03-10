3c441707ae35bbf49a73392ecf161ba683c78ff3
Cappy <cappy.pugliese@gmail.com>
2026-03-09 20:51:08 -0400

    updated feems py

--- code/python/feems/01_feems_no-washington.py
+++ code/python/feems/01_feems_no-washington.py
@@ -26,6 +26,7 @@ plt.rcParams["font.sans-serif"] = "Arial"
 
 ############ set variables
 data_path = '/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/Dagilis-lab/WNS-project/data/07_feems'
+feems_package_path ='/Users/caprinapugliese/.my-bin/miniconda3/envs/feems/lib/python3.12/site-packages/feems/data'
 
 ############ read in files
 # read the genotype data and mean impute missing data
@@ -38,15 +39,13 @@ print("n_samples={}, n_snps={}".format(genotypes.shape[0], genotypes.shape[1]))
 ############ set up graph
 # setup graph
 coord = np.loadtxt("{}/no-washington_coords.txt".format(data_path))  # sample coordinates
-outer = np.loadtxt("{}/wolvesadmix.outer".format(data_path))  # outer coordinates
-grid_path = "{}/grid_100.shp".format(data_path)  # path to discrete global grid
+grid_path = "{}/grid_100.shp".format(feems_package_path)  # path to discrete global grid
 
 # graph input files
 outer, edges, grid, _ = prepare_graph_inputs(coord=coord, 
                                              ggrid=grid_path,
                                              translated=True, 
-                                             buffer=0,
-                                             outer=outer)
+                                             buffer=1)
 ############ making graph
 plt.figure(dpi=200, figsize=(8,6))
 projection = ccrs.AzimuthalEquidistant(central_longitude=-100)
@@ -72,3 +71,19 @@ for edge in edges:
 # add sample points
 ax.scatter(coord[:, 0], coord[:, 1], s=8, color='black', zorder=2,transform=ccrs.PlateCarree(), label='sample points')
 plt.legend()
+
+############ SpatialGraph
+sp_graph = SpatialGraph(genotypes, coord, grid, edges, scale_snps=True)
+projection = ccrs.AzimuthalEquidistant(central_longitude=-100)
+
+## making map
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=10, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map(longlat=True)
+v.draw_samples()
+v.draw_edges(use_weights=False)
+v.draw_obs_nodes(use_ids=False)
