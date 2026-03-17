b1d0794084a08ba845841d272a849ba58b947f59
Cappy <cappy.pugliese@gmail.com>
2026-03-17 16:18:05 -0400

    finished feemsmix

--- code/python/feems/01_feems_no-washington.py
+++ code/python/feems/01_feems_no-washington.py
@@ -130,7 +130,41 @@ plt.xlabel('Fitted distance'); plt.ylabel('Genetic distance')
 plt.title(r"$\tt{FEEMS}$ fit with estimated node-specific variances")
 
 
-
 ############################
 ### left off at fit FEEMSmix
 ############################
+
+# choose a specified level 
+outliers_df = sp_graph.extract_outliers(0.01)
+
+# visualizing the outlier demes on the map
+fig = plt.figure(dpi=200)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map(); v.draw_edges(use_weights=False)
+# ~NEW~ function
+v.draw_outliers(outliers_df)
+# using deme IDs since all results will be represented with these numbers
+v.draw_obs_nodes(use_ids=True)
+
+# recommended to start with K=10 (nedges is K)
+seq_results = sp_graph.sequential_fit(
+    outliers_df=outliers_df, 
+    lamb=2.15, lamb_q=1.0, optimize_q='n-dim', 
+    nedges=10, top=5
+)
+
+# visualizing the LREs as arrows
+fig = plt.figure(dpi=250)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map(); v.draw_edges(use_weights=True); v.draw_edge_colorbar(); v.draw_obs_nodes()
+v.draw_LREs(seq_results); v.draw_c_colorbar()
+
+plot_FEEMSmix_summary(seq_results, sequential=True)
