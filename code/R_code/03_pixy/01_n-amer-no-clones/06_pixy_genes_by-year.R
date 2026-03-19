3af7c5f11a3877ae5045767bddb0bbaba17eab5e
Cappy <cappy.pugliese@gmail.com>
2026-03-18 20:18:52 -0400

    merged gene info with data

--- code/R_code/03_pixy/01_n-amer-no-clones/06_pixy_genes_by-year.R
+++ code/R_code/03_pixy/01_n-amer-no-clones/06_pixy_genes_by-year.R
@@ -17,15 +17,17 @@ theme <- theme_cowplot()
 cols <- c("#064061","#0072B2","#56B4E9", "#B4E1FB", "#009E73","#DACE1E","#E69F00","#D55E00","#9C4907")
 
 ###### fixing fst NAs ######
-dxy_df <- dxy_df |> mutate(.before=pop1 ,row_order = as.numeric(gsub("\\D+", "",(paste(chromosome,window_pos_1,pop1,pop2, sep = "_"))))) |> arrange(row_order)
+dxy_df <- dxy_df |> mutate(.before=pop1 ,row_order = (paste(chromosome,window_pos_1,pop1,pop2, sep = "_"))) |> arrange(row_order)
+dxy_df <- dxy_df |> select(row_order,pop1,pop2,chromosome,window_pos_1,avg_dxy)
 
-fst_df <- fst_df |> mutate(.before=pop1 , row_order = as.numeric(gsub("\\D+", "",(paste(chromosome,window_pos_1,pop1,pop2, sep = "_"))))) |> arrange(row_order)
-
-dxy_df <- dxy_df |> select(row_order,avg_dxy,no_sites,count_diffs,count_comparisons,count_missing)
+fst_df <- fst_df |> mutate(.before=pop1 , row_order = (paste(chromosome,window_pos_1,pop1,pop2, sep = "_"))) |> arrange(row_order)
+fst_df <- fst_df |> select(row_order,pop1,pop2,chromosome,window_pos_1,avg_hudson_fst)
 
 merged_fst_dxy <- left_join(fst_df, dxy_df, by = "row_order")
 
-selected_fst_dxy <- merged_fst_dxy |> select(row_order, pop1, pop2, chromosome, window_pos_1, avg_hudson_fst, avg_dxy)
+selected_fst_dxy <- merged_fst_dxy |> select(row_order, pop1.x, pop2.x, chromosome.x, window_pos_1.x, avg_hudson_fst, avg_dxy)
+
+selected_fst_dxy <- selected_fst_dxy |> rename("pop1" = "pop1.x","pop2" = "pop2.x", "chromosome" = "chromosome.x", "window_pos_1" = "window_pos_1.x")
 
 # conditional statement: sets fst NAs to zero if dxy is also zero
 selected_fst_dxy$avg_hudson_fst[is.na(selected_fst_dxy$avg_hudson_fst) & selected_fst_dxy$avg_dxy == 0] <- 0
@@ -60,21 +62,26 @@ year1_compar_plot
 ######################
 # making a new graph
 ######################
+fst_dxy_df <- fst_dxy_df |> mutate(.before=pop1 ,gene_order = (paste(chromosome,window_pos_1, sep = "_")))
+gene_info <- gene_info |> mutate(.before=scaffold ,gene_order = (paste(scaffold,start, sep = "_")))
+gene_info <- gene_info |> arrange(gene_order)
 
-gene_df <- 
+gene_df <- left_join(fst_dxy_df, gene_info, by = "gene_order")
+gene_df <- gene_df |> select(row_order,pop1,pop2,scaffold, window_pos_1,gene_id, avg_hudson_fst, avg_dxy)
 
-highest_fst_genes <- fst_df |> mutate(.before=pop1, chromosome_number = as.numeric(gsub(".1","",(gsub("NW_","",paste(chromosome))),fixed =TRUE))) |> arrange(chromosome_number, window_pos_1)
-
-
-|> filter(avg_hudson_fst == 1) |> count(pop2)
+highest_fst_genes <- gene_df |> filter(avg_hudson_fst == 1) |> count(gene_id)
 # 52 genes
 # all compared to pop9 (2016)
 
-ggplot(highest_fst_genes, aes(x=chromosome, y=n)) +
-  geom_point() +
-  labs(title = "Average Fst > 1 Gene Count per Scaffold", x = "Genome Scaffolds", y = "Number of Genes with Fst > 1", color = "Year") +
-  scale_color_manual(values = cols) +
-  theme +
-  theme(axis.title.x=element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank())
+ggplot(highest_fst_genes, aes(x=n)) +
+  geom_histogram() +
+
+ggplot(highest_fst_genes, aes(x=gene_id, y=n)) +
+  geom_point()
+
+
+### list of genes that have the highest fst (fst=1)
+### look at what the genes are --> uniprot
+  ### https://www.uniprot.org/
 
 
