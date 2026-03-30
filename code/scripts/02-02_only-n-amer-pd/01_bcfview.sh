4f7e78229980e03ec46d121d8e87915bd12f1c55
Cappy <cappy.pugliese@gmail.com>
2026-03-30 17:10:11 -0400

    trying to get better snp estimate

--- code/scripts/02-02_only-n-amer-pd/01_bcfview.sh
+++ code/scripts/02-02_only-n-amer-pd/01_bcfview.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=5G
 #SBATCH --partition=general
 #SBATCH --qos=general
-#SBATCH -o %x_%j.out
-#SBATCH -e %x_%j.err
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
 
 ########### script start
 
@@ -15,19 +15,19 @@ hostname
 date
 
 ## variables
-INDIR=/home/FCAM/cpugliese/lab_wns/05_vcfs/01_orig-vcfs/01_all-samples/ploidy-1
+INDIR=/home/FCAM/cpugliese/lab_wns/05_vcfs/01_orig-vcfs/02_only-pd/01_ploidy1
 OUTDIR=/home/FCAM/cpugliese/lab_wns/05_vcfs/01_orig-vcfs/03_only-n-amer-pd
-SAMPLES=/home/FCAM/cpugliese/wns/04_vcf/scripts/only-n-amer-pd/n-amer_pd.txt
+SAMPLES=/home/FCAM/cpugliese/wns/04_vcf/02_other-vcf-scritps/02_only-n-amer-pd/n-amer_pd.txt
 OUTDIR2=/home/FCAM/cpugliese/lab_wns/05_vcfs/03_filtered-vcfs
 
 ## load modules
-module load bcftools/1.20
+module load bcftools/1.19
 
 ## run bcftools to view north american samples only
 bcftools view -S $SAMPLES \
 -Oz \
--o $OUTDIR/n-amer_ploidy1_pd.vcf.gz \
-$INDIR/pd_ploidy-1.vcf.gz
+-o $OUTDIR/snp-count_n-amer_ploidy1_pd.vcf.gz \
+$INDIR/only-pd.vcf.gz
 
 
 ## filter using bcftools
@@ -41,7 +41,7 @@ bcftools view -S $SAMPLES \
 -m2 \
 -M2 \
 -Oz \
--o $OUTDIR2/n-amer-pd-only_filtered.vcf.gz \
-$INDIR/pd_ploidy-1.vcf.gz
+-o $OUTDIR2/snp-count_n-amer-pd-only_filtered.vcf.gz \
+$INDIR/only-pd.vcf.gz
 
 ########### script end
