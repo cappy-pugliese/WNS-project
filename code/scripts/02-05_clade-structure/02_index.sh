7a47a133830ee94bc5abfe0187a454867bc863b1
Cappy <cappy.pugliese@gmail.com>
2026-04-01 17:57:06 -0400

    making new vcf to look at clade structure

---
+++ code/scripts/02-05_clade-structure/02_index.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=bcftools
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 2
+#SBATCH --mem=5G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
+
+########### script start
+
+hostname
+date
+
+## load modules
+module load bcftools/1.19
+
+## set variables
+INDIR=/home/FCAM/cpugliese/lab_wns/05_vcfs/03_filtered-vcfs
+VCF=clade-structure-pds_filtered.vcf.gz
+
+cd $INDIR
+
+## bcftools
+bcftools index \
+$VCF
+
+########### script end
